139517b8aa0e4e4c29c4d8e70db14f853b9cd5c2
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-11-05 20:33:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+y = 123
+
+y = "hola mundo"
+
+# Esto es Python 
+def sumar(a, b):
+    """ Sumar dos numeros a y b """
+    z =  a + b
+    return z
+
+def restar(a, b): 
+    return a - b
+
+super_lista = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+print(3 in super_lista)
+print(4 in super_lista)
+
+
+count = 0
+
+while count < 5:
+    print(count)
+    count = int(input("Ingrese un numero: "))
+
+print("Fin del programa")
